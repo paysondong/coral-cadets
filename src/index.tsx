ae170c3d6e2a899b3468ed38361436aad136f1a0
paysondong <744629@qq.com>
2026-09-22 10:49:28 +0800

    Prepare v0.2.0: src

--- src/index.tsx
+++ src/index.tsx
@@ -6,34 +6,40 @@ import Game from "./game/Game";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./game/SignUp";
 import SignIn from "./game/SignIn";
-import { GoogleOAuthProvider, useGoogleLogin } from '@react-oauth/google';
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-
 const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+const oauthClient =
+  clientId ||
+  "726356794954-9a5jbupct8timuemc39meojkdkul1afg.apps.googleusercontent.com";
 
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={
-        <GoogleOAuthProvider clientId={clientId || "726356794954-9a5jbupct8timuemc39meojkdkul1afg.apps.googleusercontent.com"}>
-          <SignIn />
-        </GoogleOAuthProvider>
-      }/>
-      <Route path="/register" element={
-        <GoogleOAuthProvider clientId={clientId || "726356794954-9a5jbupct8timuemc39meojkdkul1afg.apps.googleusercontent.com"}>
-          <SignUp />
-        </GoogleOAuthProvider>
-      } />
+      <Route path="/" element={<Game />} />
       <Route path="/game" element={<Game />} />
+      <Route
+        path="/signin"
+        element={
+          <GoogleOAuthProvider clientId={oauthClient}>
+            <SignIn />
+          </GoogleOAuthProvider>
+        }
+      />
+      <Route
+        path="/register"
+        element={
+          <GoogleOAuthProvider clientId={oauthClient}>
+            <SignUp />
+          </GoogleOAuthProvider>
+        }
+      />
     </Routes>
   </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
